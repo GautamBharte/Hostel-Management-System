18c67766701b8ecd5f250881c4efda534a1fc03c
Gautam Kailas Bharte <gautambharte123@gmail.com>
2022-08-18 09:03:30 +0530

    Rendered students data to dashboard

--- KKWIEER_Hostel/models.py
+++ KKWIEER_Hostel/models.py
@@ -4,12 +4,18 @@ from django.db import models
 # Create your models here.
 
 class Student(models.Model):
+    FEES_STATUS = (
+        ('Pending', 'Pending'),
+        ('Paid', 'Paid')
+    )
+    
     prn = models.CharField(max_length=12, null=True)
     name = models.CharField(max_length=200, null=True)
     email = models.CharField(max_length=200, null=True)
     branch = models.CharField(max_length=50, null=True)
     room_no = models.CharField(max_length=5, null=True)
     date_created = models.DateTimeField(auto_now_add=True)
+    fees_status = models.CharField(max_length=200, null=True, choices=FEES_STATUS)
 
     def __str__(self) -> str:
         return self.name
--- KKWIEER_Hostel/views.py
+++ KKWIEER_Hostel/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import *
 
 # Create your views here.
 def home(request):
     return render(request, 'accounts/main.html')
 
 def user(request):
-    return render(request, 'accounts/dashboard.html')
+    students = Student.objects.all()
+    total_students = students.count()
+    fees_pending = students.filter(fees_status='Pending').count()
+    fees_paid = students.filter(fees_status='Paid').count()
+    
+    context = {
+        'students' : students,
+        'total_students' : total_students,
+        'fees_pending' : fees_pending,
+        'fees_paid' : fees_paid
+    }
+    
+    return render(request, 'accounts/dashboard.html', context)
 
 def profile(request):
     return render(request, 'accounts/profile.html')
