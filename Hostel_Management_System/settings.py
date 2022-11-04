c4fd07e708136d5238874748ba9f1357043266cc
Gautam Kailas Bharte <gautambharte123@gmail.com>
2022-11-04 19:56:48 +0530

    Forgot password with SMTP server

--- Hostel_Management_System/settings.py
+++ Hostel_Management_System/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'KKWIEER_Hostel',
+    'KKWIEER_Hostel.apps.KkwieerHostelConfig',
     'django_filters',
 ]
 
@@ -133,3 +133,13 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
+
+#SMTP Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'kkwieerhostel@gmail.com'
+EMAIL_HOST_PASSWORD = 'dlqltnerliuebbfe'
--- KKWIEER_Hostel/urls.py
+++ KKWIEER_Hostel/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+
+from django.contrib.auth import views as auth_views
+
 from . import views
 
 urlpatterns = [
@@ -14,5 +17,18 @@ urlpatterns = [
     path('add_student/', views.addStudent, name='add_student'),
     path('update_student/<str:pk>', views.updateStudent, name='update_student'),
     path('delete_student/<str:pk>', views.deleteStudent, name='delete_student'),
+
+    path('reset_password/', 
+         auth_views.PasswordResetView.as_view(template_name="accounts/password_reset.html"),
+         name="reset_password"),
+    path('reset_password_sent/', 
+         auth_views.PasswordResetDoneView.as_view(template_name="accounts/password_reset_sent.html"), 
+         name="password_reset_done"),
+    path('reset/<uidb64>/<token>', 
+         auth_views.PasswordResetConfirmView.as_view(template_name="accounts/password_reset_form.html"), 
+         name="password_reset_confirm"),
+    path('reset_password_complete/', 
+         auth_views.PasswordResetCompleteView.as_view(template_name="accounts/password_reset_done.html"), 
+         name="password_reset_complete"),
 
 ]
